d403572fb477d80cc94b389912a74e9c698da730
moe93 <m.odeh93@gmail.com>
2018-07-19 16:54:23 -0400

    Make EEPROM_write() faster, better, strongergit add .!

--- Software/Arduino/IMU_2address_4sensors_cs1/EEPROMAnything.h
+++ Software/Arduino/IMU_2address_4sensors_cs1/EEPROMAnything.h
@@ -13,21 +13,30 @@ struct config_t
 // =========================  Write EEPROM  =========================
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
-    const byte* p = (const byte*)(const void*)&value;
-    unsigned int i;
-    for (i = 0; i < sizeof(value); i++)
-          EEPROM.write(ee++, *p++);
-    return i;
+  const byte* p = (const byte*)(const void*)&value;
+  unsigned int i;
+  for (i = 0; i < sizeof(value); i++)
+  {
+    if ( EEPROM.read(ee) == *p )
+    {
+      ee++; p++;
+    }
+    else
+    {
+      EEPROM.write(ee++, *p++);  // Only write the data if it is different to what's there
+    }
+  }
+  return i;
 }
 
 // =========================  Read  EEPROM  =========================
 template <class T> int EEPROM_readAnything(int ee, T& value)
 {
-    byte* p = (byte*)(void*)&value;
-    unsigned int i;
-    for (i = 0; i < sizeof(value); i++)
-          *p++ = EEPROM.read(ee++);
-    return i;
+  byte* p = (byte*)(void*)&value;
+  unsigned int i;
+  for (i = 0; i < sizeof(value); i++)
+    *p++ = EEPROM.read(ee++);
+  return i;
 }
 
 // ================  Read  EEPROM & print to Serial  ================
