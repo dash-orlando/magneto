99bcef5dcb7a5968814d8fc11da168bc15eb5f8b
fluviolobo <fluvio.lobo@gmail.com>
2018-10-11 16:33:49 -0400

    Created a 4MAG directory within the builds folder. 4-sensor-arrays are planned as prototypes.

---
+++ Builds/4MAG/Software/Arduino/IMU_2address_cs1/pairSelect.h
@@ -0,0 +1,32 @@
+/*
+ * Switches between all the available sensors using the multiplexer
+ */
+
+// =======================    Select IMU pair       ====================
+void pairSelect(int desiredPair) {
+  int pair = desiredPair;
+  if (pair == 1) {
+    //s.t. the selection lines hit Y0, at 000
+    digitalWrite(S0, LOW);
+    digitalWrite(S1, LOW);
+    delay(DEBOUNCE);
+  } else if (pair == 2) {
+    //s.t. the selection lines hit Y1, at 001
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, LOW);
+    delay(DEBOUNCE);
+  } else if (pair == 3) {
+    //s.t. the selection lines hit Y2, at 010
+    digitalWrite(S0, LOW);
+    digitalWrite(S1, HIGH);
+    delay(DEBOUNCE);
+  } else if (pair == 4) {
+    //s.t. the selection lines hit Y3, at 011
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, HIGH);
+    delay(DEBOUNCE);
+  } else {
+    Serial.println( F("Error. This sensor pair doesn't exist!") );
+    while (1);
+  }
+};
